74899a683e39865bc33738217373632d35925e60
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2021-10-29 12:43:37 -0700

    Update schedule_sjf.cpp

--- schedule_sjf.cpp
+++ schedule_sjf.cpp
@@ -5,12 +5,49 @@
 void schedule_sjf::add(char* name, int priority, int burst){
 	task temp;
 	temp.name = name, temp.priority = priority, temp.burst = burst;
-	scheduled_tasks.push_back(temp);
+		scheduled_tasks.push_back(temp);
+}
+
+//sort fxn for 
+void schedule_sjf::sort( vector<task> &v ){
+	int count = 0;
+	
+	for(auto i=v.begin(), i < v.size()-1, i++){
+		task temp1 = *(i);
+		for(auto j= v.begin()+1; j < v.size()-1; j++){
+			task temp2 = *(j);
+			if(temp1[j].burst > temp2[j+1].burst){
+				std::swap(v.at(j),v.at(j+1));
+			}
+		}
+	}
+		/**void bubbleSort(vector<T> &vec) {
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        for (size_t j = 0; j < vec.size() - i - 1; ++j) {
+            if (vec.at(j) > vec.at(j + 1))
+                std::swap(vec.at(j), vec.at(j + 1));
+        }
+    }
+*/
+		
+	}
+	
+	
 }
 
 /**
  * Run the SJF scheduler
  */
 void schedule_sjf::schedule(){
-	// TODO: add your implementation here
+	int i = 0;
+	for (auto it = scheduled_tasks.begin(); it != scheduled_tasks.end(); it++) {
+		task temp = *(it);
+		std::cout << "Processing Task: " << temp.name << " " << temp.priority << " " << temp.burst << " " << '\n';
+		
+		//wait time for SJF
+		int prev_time = cpu_handle.waitTime[i];
+		cpu_handle.waitTime[++i] = temp.burst + prev_time;
+
+		cpu_handle.displayAvgWait();
+		cpu_handle.displayUtilization();
 }
